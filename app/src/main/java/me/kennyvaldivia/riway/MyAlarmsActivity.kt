41d18ed554686c79f45a8684a8d6d7ba4b525efc
Kenny Valdivia <k_valdivia@gmx.com>
2020-07-05 12:06:04 -0500

    First commit

---
+++ app/src/main/java/me/kennyvaldivia/riway/MyAlarmsActivity.kt
@@ -0,0 +1,11 @@
+package me.kennyvaldivia.riway
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyAlarmsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_alarms)
+    }
+}
